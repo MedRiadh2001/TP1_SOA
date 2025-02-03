acd05c24e5425af91d2e3b6d05b8a614cd90d949
MedRiadh2001 <mohamedriadh.essridi@polytechnicien.tn>
2025-02-03 21:14:44 +0100

    test de de Weather avec axios

---
+++ test_axios.js
@@ -0,0 +1,20 @@
+const axios = require("axios");
+
+const API_KEY = "38f9264b8e345e5059d64b5e08c19663";
+const BASE_URL =
+  "http://api.openweathermap.org/data/2.5/weather?appid=" + API_KEY + "&q=";
+
+async function getWeatherData(city) {
+  const url = BASE_URL + city + "&units=metric&lang=fr";
+  try {
+    const response = await axios.get(url);
+    const data = response.data;
+    console.log("Weather Description:", data.weather[0].description);
+    console.log("Temperature:", data.main.temp + "°C");
+    console.log("Humidity:", data.main.humidity + "%");
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+getWeatherData("Sousse");
